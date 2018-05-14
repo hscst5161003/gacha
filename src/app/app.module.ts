642bdd0ac7fbf307e20ac173d91466255ef2a0e8
youthan <mine4423@outlook.jp>
2018-05-15 08:38:35 +0900

    iiiiiiii

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatToolbarModule, MatSidenavModule,
+         MatIconModule, MatListModule, MatTabsModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
@@ -32,6 +33,7 @@ import { GachaListComponent } from './gacha-list/gacha-list.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
